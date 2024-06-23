d1f318f8eaa6f7ca71246ac67030a7cab49b8143
mr0bn0xi0us <nbhellossss@gmail.com>
2024-06-23 17:55:54 +0545

    Updated .env file

--- app/config.py
+++ app/config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 class Settings:
     pguser: str = os.getenv("PGUSER")
-    postgres_password = os.getenv("POSTGRES_PASSWORD")
+    postgres_password = os.getenv("PGPASSWORD")
     railway_private_domain = os.getenv("RAILWAY_PRIVATE_DOMAIN")
     pgdatabase = os.getenv("PGDATABASE")
-    database_private_url: str = f"postgresql://{pguser}:{postgres_password}@{railway_private_domain}:5432/{pgdatabase}"
+    database_private_url: str = os.getenv("DATABASE_PRIVATE_URL")
     secret_key: str = os.getenv("SECRET_KEY")
     algorithm: str = os.getenv("ALGORITHM")
     access_token_expire_minutes: int = "ACCESS_TOKEN_EXPIRE_MINUTES"
