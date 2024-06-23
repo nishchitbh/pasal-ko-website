950375737d9cbfb5e6fc18c49e74e794abb65036
mr0bn0xi0us <nbhellossss@gmail.com>
2024-06-23 15:34:18 +0545

    Edited config.py

--- app/config.py
+++ app/config.py
@@ -1,21 +1,20 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-
-    class Config:
-        env_file = "../.env"
+    database_hostname: str = os.getenv("DATABASE_HOSTNAME")
+    database_port: str = os.getenv("DATABASE_PORT")
+    database_password: str = os.getenv('DATABASE_PASSWORD')
+    database_name: str = os.getenv('DATABASE_NAME')
+    database_username: str = os.getenv('DATABASE_USERNAME')
+    secret_key: str = os.getenv('SECRET_KEY')
+    algorithm: str = os.getenv('ALGORITHM')
+    access_token_expire_minutes: int = ('ACCESS_TOKEN_EXPIRE_MINUTES')
 
 
 settings = Settings()
+
